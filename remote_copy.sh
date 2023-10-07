bd77bc2be46bb50c19d1dd8206c2da0663e5dd08
Sandeep084b <108270583+Sandeep084b@users.noreply.github.com>
2023-10-07 12:51:56 +0530

    Update remote_copy.sh

--- remote_copy.sh
+++ remote_copy.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
-#get modified files info as string
+# Input string containing values separated by a delimiter (e.g., comma)
 input_string="$modifiedfiles"
 
-#Define the delimiter
+# Define the delimiter
 delimiter=","
 
-# Splitthe input string into an using the delimiter
+# Split the input string into an array using the delimiter
 IFS="$delimiter" read -ra filenames <<< "$input_string"
 
 # Create an associative array to store unique values
 declare -A unique_values
 
-# Ignore .github and root folders
 exclude1=".github"
 exclude2="."
 
-# Iterate through the values to copy unique one into the array
+# Iterate through the values to copy unique ones into the associative array
 for filename in "${filenames[@]}"; do
 	folder_name="$(dirname "$filename")"
-	base_dir=$(echo "$folder_name" | awk -F'/' '{print $1}')
-	if [[ "$base_dir" == "$exclude1" ]] || [[ "$base_dir" == "exclude2" ]]; then
+	#echo "$folder_name"
+    # Use the value as a key in the associative array to ensure uniqueness
+	base_dir=$(echo "$folder_name" | awk -F'/' '{print $1}')	
+	#echo "$base_dir"
+	if  [[ "$base_dir" == "$exclude1" ]] || [[ "$base_dir" == "$exclude2" ]]; then		
 		continue
 	else
-		unique_values["$base_dir"]=1
+		unique_values["$base_dir"]=0
 	fi
 done
 
+# Convert the associative array keys (unique values) into a regular indexed array
 if [ "${#unique_values[@]}" != 0 ]; then
 	unique_array=("${!unique_values[@]}")
-	
+
+# Print the unique values (optional)
 	for value in "${unique_array[@]}"; do
 		echo "$value"
 	done
 else
-	echo "No Deployment Needed"
+	echo "No deployment needed"
 fi
-
-	
